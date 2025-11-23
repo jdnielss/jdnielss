2b24993276b5524233d762880135dabc7bd6f7f5
Juan Daniel <mail.jdnielss@gmail.com>
2025-11-24 00:52:07 +0700

    refactor(blog-section): remove unused useState import

--- components/sections/blog-section.tsx
+++ components/sections/blog-section.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { motion } from 'motion/react'
 import Link from 'next/link'
-import { useState } from 'react'
 import { BLOG_POSTS } from '@/lib/blog'
 
 export function BlogSection() {
